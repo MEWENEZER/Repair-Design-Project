fcf1bef5bd5b8ae1ad4caf97096e8e7886802630
Dmitriy <ubersweet@yandex.com>
2023-01-01 20:49:34 +0300

    Update Pattern '%title'
    - Edit 'background'

--- IC_Repair_Design_Project/script/script.js
+++ IC_Repair_Design_Project/script/script.js
@@ -11,7 +11,7 @@ sectionAboutButtons.onclick = function (event) {
   let target = event.target; // Where was a click?
 
   highlight(target);
-}
+};
 
 function highlight(btn) {
   if (selectedActive) {
@@ -27,14 +27,17 @@ function changePhoto(id) {
   switch (id) {
     case '1':
       sectionAboutPhoto.src = '/IC_Repair_Design_Project/images/main-bg-3.png';
+      sectionAboutPhoto.alt = 'Moscow, Tverskaya';
       break;
 
     case '2':
       sectionAboutPhoto.src = '/IC_Repair_Design_Project/images/main-bg-2.png';
+      sectionAboutPhoto.alt = 'Moscow, Arbat';
       break;
 
     case '3':
       sectionAboutPhoto.src = '/IC_Repair_Design_Project/images/main-bg-1.png';
+      sectionAboutPhoto.alt = 'Moscow, Tagansky';
       break;
   }
 }
